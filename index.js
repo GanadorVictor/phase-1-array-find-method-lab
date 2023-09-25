ce9e61f5eb5d02140f6f7de88f8b75f58ee24029
Victor Muturi <muturivictor21@gmail.com>
2023-09-25 12:15:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(entry => entry.result === "W" && entry.team === "Denver Broncos");
+    return win ? win.year : undefined;
+  }
+  
+  module.exports = superbowlWin;
+  
